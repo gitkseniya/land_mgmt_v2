c1f9119d6bec32e5cb2cedcefaccf7de745bf154
Kseniya K <kseniya.kudelich@gmail.com>
2021-12-29 14:44:21 -0700

    Remove polls

--- land_mgmt_v2/settings.py
+++ land_mgmt_v2/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'landm.apps.LandmConfig',
-    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
